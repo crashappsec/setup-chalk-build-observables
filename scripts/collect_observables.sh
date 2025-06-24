99f1298b8ed5c7c50f3d8f0e84e874bb12e11b83
Theofilos Petsios <theofilos@crashoverride.com>
2025-06-24 09:11:13 -0400

    use find

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -4,20 +4,15 @@ set -eu
 
 echo "parsing observables data from /mnt/curiosity"
 
-for prefix in "$(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl | cut -d'_' -f1-3)"; do
-    echo "Parsing data for $prefix"
-done
-
-ls /mnt/curiosity/observables/
-
-for prefix in "$(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl | cut -d'_' -f1-3)"; do
-    /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." ${prefix}_events.jsonl) \
-        --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." ${prefix}_metadata.jsonl) \
+find /mnt/curiosity/observables/ -maxdepth 1 -type f -name "${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl" | while read -r file; do
+    prefix=$(basename "$file" | cut -d'_' -f1-3)
+    echo "parsing $prefix"
+    /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/${prefix}_events.jsonl) \
+        --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/${prefix}_metadata.jsonl) \
         '{events: $events[], metadata: $metadata[]}' \
-        > "${prefix}_pstree.json"
+        > "/mnt/curiosity/observables/${prefix}_pstree.json"
 done
 
-ls /mnt/curiosity/observables/
 
 /mnt/curiosity/co-jq -cn \
   --slurpfile pstrees <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}_*pstree.json) \
