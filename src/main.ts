d43721b819fa6a8fe96c886568eeaf49eb71b22b
Theofilos Petsios <theofilos@crashoverride.com>
2025-06-24 09:15:40 -0400

    run chalk env

--- src/main.ts
+++ src/main.ts
@@ -11,54 +11,55 @@ import { promisify } from "util";
 const streamPipeline = promisify(pipeline);
 
 async function run(): Promise<void> {
-  try {
-    const url: string = core.getInput("curiosity_archive_url");
-    const tmp = fs.mkdtempSync(path.join(os.tmpdir(), "curiosity-"));
-    const setup = path.join(tmp, "curiosity-installer", "setup.sh");
+    try {
+        const url: string = core.getInput("curiosity_archive_url");
+        const tmp = fs.mkdtempSync(path.join(os.tmpdir(), "curiosity-"));
+        const setup = path.join(tmp, "curiosity-installer", "setup.sh");
 
-    core.info(`Downloading curiosity archive`);
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch curiosity archive: ${response.statusText}`,
-      );
+        core.info(`Downloading curiosity archive`);
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(
+                `Failed to fetch curiosity archive: ${response.statusText}`,
+            );
+        }
+
+        core.info(`Unpacking curiosity archive to ${tmp}`);
+        await streamPipeline(response.body!, tar.x({ cwd: tmp, gzip: true }));
+
+        core.info(`Setting up build observables via ${setup}`);
+        execSync(`sh ${setup}`, { stdio: "inherit" });
+    } catch (error) {
+        core.setFailed(`${(error as any)?.message ?? error}`);
     }
-
-    core.info(`Unpacking curiosity archive to ${tmp}`);
-    await streamPipeline(response.body!, tar.x({ cwd: tmp, gzip: true }));
-
-    core.info(`Setting up build observables via ${setup}`);
-    execSync(`sh ${setup}`, { stdio: "inherit" });
-  } catch (error) {
-    core.setFailed(`${(error as any)?.message ?? error}`);
-  }
 }
 
 async function cleanup(): Promise<void> {
-  try {
-    core.info(`Collecting build observable data...`);
+    try {
+        core.info(`Collecting build observable data...`);
 
-    const scriptPath = path.join(
-      __dirname,
-      "../scripts/collect_observables.sh",
-    );
+        const scriptPath = path.join(
+            __dirname,
+            "../scripts/collect_observables.sh",
+        );
 
-    if (!fs.existsSync(scriptPath)) {
-      throw new Error(`Post script not found at: ${scriptPath}`);
+        if (!fs.existsSync(scriptPath)) {
+            throw new Error(`Post script not found at: ${scriptPath}`);
+        }
+
+        execSync(`bash ${scriptPath}`, { stdio: "inherit" });
+        core.info(`Done emitting observables json - calling chalk env`);
+        execSync(`bash "chalk env"`, { stdio: "inherit" });
+    } catch (error) {
+        core.warning(`${(error as any)?.message ?? error}`);
     }
-
-    execSync(`bash ${scriptPath}`, { stdio: "inherit" });
-    core.info(`Done`);
-  } catch (error) {
-    core.warning(`${(error as any)?.message ?? error}`);
-  }
 }
 
 // Main
 if (!stateHelper.IsPost) {
-  run();
+    run();
 }
 // Post
 else {
-  cleanup();
+    cleanup();
 }
