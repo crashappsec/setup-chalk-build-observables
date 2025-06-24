53b066539a206e0e58a5b5d11f21c2f279d1d7fb
Theofilos Petsios <theofilos@crashoverride.com>
2025-06-24 08:48:45 -0400

    nit

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -2,17 +2,20 @@
 
 set -eu
 
+echo "parsing observables data from /mnt/curiosity"
+
 for prefix in "$(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl | cut -d'_' -f1-3)"; do
-  /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." "${prefix}_events.jsonl") \
-       --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." "${prefix}_metadata.jsonl") \
-       '{events: $events[], metadata: $metadata[]}' \
-       > ${prefix}_pstree.json
+    echo "Parsing data for $prefix"
+    /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." "${prefix}_events.jsonl") \
+        --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." "${prefix}_metadata.jsonl") \
+        '{events: $events[], metadata: $metadata[]}' \
+        > ${prefix}_pstree.json
 done
 
 ls /mnt/curiosity/observables/
 
 /mnt/curiosity/co-jq -cn \
-  --slurpfile pstrees <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_pstree.json) \
+  --slurpfile pstrees <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}_*pstree.json) \
   --slurpfile hostinfo <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/host_info.json) \
   '{pstrees: $pstrees[], hostInfo: $hostinfo[]}' \
   > /mnt/curiosity/observables/${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}-observables.json
