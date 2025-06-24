a2f36d3e40b4ac541d44c62568db2598ce15766e
Theofilos Petsios <theofilos@crashoverride.com>
2025-06-24 05:01:03 -0400

    check output

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -1,18 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-set -e
+set -eu
 
-echo "Running observables collector"
+echo "Running observables collector inside /mnt/curiosity"
+ls /mnt/curiosity
+echo "Observables contents"
+ls /mnt/curiosity/observables
+echo "------1111111111111111111----"
 
 for prefix in $(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}*_metadata.jsonl | cut -d'_' -f1-2); do
-  jq -n --slurpfile events <(jq --slurp "." ${prefix}_events.jsonl) \
-       --slurpfile metadata <(jq --slurp "." ${prefix}_metadata.jsonl) \
+  /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." ${prefix}_events.jsonl) \
+       --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." ${prefix}_metadata.jsonl) \
        '{events: $events[], metadata: $metadata[].[0]}' \
-       > ${prefix}_pstree.json
+       > /mnt/curiosity/observables/${prefix}_pstree.json
 done
 
-jq -cn \
-  --slurpfile pstrees <(jq --slurp "." /mnt/curiosity/observables/*_pstree.json) \
-  --slurpfile hostinfo <(jq --slurp "." /mnt/curiosity/observables/host_info.json) \
+echo "------22222222222222-------"
+ls /mnt/curiosity/observables
+
+/mnt/curiosity/co-jq -cn \
+  --slurpfile pstrees <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/*_pstree.json) \
+  --slurpfile hostinfo <(/mnt/curiosity/co-jq --slurp "." /mnt/curiosity/observables/host_info.json) \
   '{pstrees: $pstrees[], hostInfo: $hostinfo[].[0]}' \
-  > /mnt/curiosity/observables/${GITHUB_RUN_ID}_info.json
+  > /mnt/curiosity/observables/${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}-observables.json
+echo "------ FINAL -------"
+ls /mnt/curiosity/observables
