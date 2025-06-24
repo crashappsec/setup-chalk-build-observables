1ba5e9ec8e168db6cb0b973c527b1a4d2d0ce076
Theofilos Petsios <theofilos@crashoverride.com>
2025-06-24 09:00:13 -0400

    debug print

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -6,10 +6,15 @@ echo "parsing observables data from /mnt/curiosity"
 
 for prefix in "$(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl | cut -d'_' -f1-3)"; do
     echo "Parsing data for $prefix"
-    /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." "${prefix}_events.jsonl") \
-        --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." "${prefix}_metadata.jsonl") \
+done
+
+ls /mnt/curiosity/observables/
+
+for prefix in "$(ls /mnt/curiosity/observables/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*_metadata.jsonl | cut -d'_' -f1-3)"; do
+    /mnt/curiosity/co-jq -n --slurpfile events <(/mnt/curiosity/co-jq --slurp "." ${prefix}_events.jsonl) \
+        --slurpfile metadata <(/mnt/curiosity/co-jq --slurp "." ${prefix}_metadata.jsonl) \
         '{events: $events[], metadata: $metadata[]}' \
-        > ${prefix}_pstree.json
+        > "${prefix}_pstree.json"
 done
 
 ls /mnt/curiosity/observables/
