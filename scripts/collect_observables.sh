cd19520efe4a3101c67b9a6f41e6beee66b19eb0
Theofilos Petsios <theofilos@crashoverride.com>
2025-07-17 15:15:58 +0300

    fix: add new line after host_info

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -6,6 +6,8 @@ OBSERVABLES_DIR=/mnt/curiosity/observables
 COMBINED=${OBSERVABLES_DIR}/${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}-observables.combined
 OUTPUT=${OBSERVABLES_DIR}/${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}-observables.br
 cat ${OBSERVABLES_DIR}/host_info.json > ${COMBINED}
+# add a new line becuase host_info doesn't have it
+echo >> ${COMBINED}
 cat ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl >> ${COMBINED}
 
 /mnt/curiosity/co-brotli -q 5 -o ${OUTPUT} ${COMBINED}
