fdcd6be55edf79e87a79d422101bea2d571c257f
Vaggelis Atlidakis <vaggelis@crashoverride.com>
2026-02-11 17:29:54 +0200

    making sense of things...

--- scripts/unwrap.sh
+++ scripts/unwrap.sh
@@ -8,10 +8,9 @@ else
     echo "We will autounwrap"
 fi
 
-pwd
-ls -hlia .
-ls -hlia /mnt/curiosity
 
+# Forcing to always unwrap
+ls -hlia /mnt/curiosity
 REFCOUNT_FILE="/mnt/curiosity/refcount.txt"
 echo "1" > "$REFCOUNT_FILE"
 
@@ -23,40 +22,41 @@ fi
 
 echo "refcount file found...Will check if it's time to unwrap"
 
-am_i_root(){
-    local rval=$(whoami)
-    echo "$rval"
-    if [ "$rval" = "root" ]; then
-        return 0
-    fi
-    return 1
-}
-
-can_i_sudo(){
-    SUDO=$(command -v sudo)
-    "$SUDO" ls / > /dev/null 2>&1
-    if [ "$?" -eq 0 ]; then
-        return 0
-    fi
-    return 1
-}
-
-SUDO=
-am_i_root
-if [ "$?" -ne 0 ]; then
-    echo "I am not root"
-    can_i_sudo
-    if [ "$?" -ne 0 ]; then
-        echo "I cannot sudo"
-        return
-    fi
-    echo "I can sudo: $SUDO"
-fi
-
 flock "$REFCOUNT_FILE" sh -c '
+     am_i_root(){
+        local rval=$(whoami)
+        echo "$rval"
+        if [ "$rval" = "root" ]; then
+            return 0
+        fi
+        return 1
+    }
+
+    can_i_sudo(){
+        SUDO=$(command -v sudo)
+        "$SUDO" ls / > /dev/null 2>&1
+        if [ "$?" -eq 0 ]; then
+            return 0
+        fi
+        return 1
+    }
+
+    SUDO=
+    am_i_root
+    if [ "$?" -ne 0 ]; then
+        echo "I am not root"
+        can_i_sudo
+        if [ "$?" -ne 0 ]; then
+            echo "I cannot sudo"
+            return
+        fi
+        echo "I can sudo: $SUDO"
+    fi
+
     file="$1"
     read value < "$file"
     value=$((value - 1))
+
     if [ "$value" -ne "0" ]; then
         echo "Decrement value to $value" 
         echo "$value" > "$file"
@@ -71,6 +71,6 @@ flock "$REFCOUNT_FILE" sh -c '
         ls -hlia  /mnt/curiosity/docker-buildx.bkp
         ls -hlia  /usr/libexec/docker/cli-plugins/docker-buildx
         "$SUDO" mv /mnt/curiosity/docker-buildx.bkp /usr/libexec/docker/cli-plugins/docker-buildx
-        ls -hlia  /usr/libexec/cli-plugins/docker-buildx
+        ls -hlia  /usr/libexec/docker/cli-plugins/docker-buildx
     fi
 ' _ "$REFCOUNT_FILE"
