a38a707cf16b7c227ad5a53c32afdf89b6bbcae1
Vaggelis Atlidakis <vaggelis@crashoverride.com>
2026-03-15 05:18:07 +0200

    fixes

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -12,7 +12,6 @@ if [ "${CURIOSITY_DOOD:-}" = "1" ]; then
     # Does co-docker exist at this point? It should...
     docker ps
 
-
     # Combine and prepare all observables for the specific run-id and attempt
     docker exec co-docker \
         -e OBSERVABLES_DIR=${OBSERVABLES_DIR} \
@@ -25,14 +24,15 @@ if [ "${CURIOSITY_DOOD:-}" = "1" ]; then
                 cat "${OBSERVABLES_DIR}/host_info.json" > "${COMBINED}"
             fi
 
-            cat "${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}"*.jsonl > "${COMBINED}" && "${BROTLI_BIN}" -q 5 -o "${OUTPUT}" "${COMBINED}"
+            cat "${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}"*.jsonl >> "${COMBINED}" && "${BROTLI_BIN}" -q 5 -o "${OUTPUT}" "${COMBINED}"
         '
 
     # Grab combined observables file from host and make it visible on the container
     # running the current post-checkout action step, since this is the one that will
     # run chalk and set the data to our platform
-    docker run --rm -v ${OUTPUT}:/tmp-observables-combined docker:24-cli \
-        sh -c "tar -C /tmp-observables-combined -c . || (echo 'Host path does not exist'; exit 1)" | tar -x -C ${COMBINED}
+    mkdir -p ${OBSERVABLES_DIR}
+    docker run --rm -v ${OUTPUT}:/tmp-observables-output docker:24-cli \
+        sh -c "cat tmp-observables-output" > ${OUTPUT}
 
     ls -hlia ${OBSERVABLES_DIR}
     ls -hlia ${COMBINED}
@@ -59,5 +59,3 @@ else
     # XXX should be safe to remove these at this point
     rm ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl
 fi
-
-
