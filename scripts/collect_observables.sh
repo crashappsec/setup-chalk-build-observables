ff43f630053a3004ed3909daaefea2d9ebc3d10d
Vaggelis Atlidakis <vaggelis@crashoverride.com>
2026-03-15 05:02:59 +0200

    bugfix and nit

--- scripts/collect_observables.sh
+++ scripts/collect_observables.sh
@@ -12,6 +12,7 @@ if [ "${CURIOSITY_DOOD:-}" = "1" ]; then
     # Does co-docker exist at this point? It should...
     docker ps
 
+
     # Combine and prepare all observables for the specific run-id and attempt
     docker exec co-docker \
         -e OBSERVABLES_DIR=${OBSERVABLES_DIR} \
@@ -19,35 +20,44 @@ if [ "${CURIOSITY_DOOD:-}" = "1" ]; then
         -e BROTLI_BIN=${BROTLI_BIN} \
         -e COMBINED=${COMBINED} \
         -e OUTPUT=${OUTPUT} \
-        sh -c "cat ${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}*.jsonl > ${COMBINED} && ${BROTLI_BIN} -q 5 -o ${OUTPUT} ${COMBINED}"
+        sh -c '
+            if [ -f "${OBSERVABLES_DIR}/host_info.json" ]; then
+                cat "${OBSERVABLES_DIR}/host_info.json" > "${COMBINED}"
+            fi
+
+            cat "${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}"*.jsonl > "${COMBINED}" && "${BROTLI_BIN}" -q 5 -o "${OUTPUT}" "${COMBINED}"
+        '
 
     # Grab combined observables file from host and make it visible on the container
     # running the current post-checkout action step, since this is the one that will
     # run chalk and set the data to our platform
-    docker run --rm -v ${COMBINED}:/tmp-observables-combined docker:24-cli \
+    docker run --rm -v ${OUTPUT}:/tmp-observables-combined docker:24-cli \
         sh -c "tar -C /tmp-observables-combined -c . || (echo 'Host path does not exist'; exit 1)" | tar -x -C ${COMBINED}
+
     ls -hlia ${OBSERVABLES_DIR}
     ls -hlia ${COMBINED}
+    ls -hlia ${OUTPUT}
 
     # Clean up akin to how is done on host-native case below
     docker exec co-docker
         -e COMBINED=${COMBINED} \
         -e OBSERVABLES_DIR=${OBSERVABLES_DIR} \
         -e OBSERVABLES_PREFIX=${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT} \
-        sh -c "rm ${COMBINED} && rm ${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}*.jsonl"
+        sh -c '
+            rm "${COMBINED}" && rm "${OBSERVABLES_DIR}/${OBSERVABLES_PREFIX}*.jsonl"
+        '
+else
+    if [ -f "${OBSERVABLES_DIR}/host_info.json" ]; then
+        cat "${OBSERVABLES_DIR}/host_info.json" > "${COMBINED}"
+        echo >> "${COMBINED}"
+    fi
 
-    # Exit here, when we are done; what should happen next is chalk to pull the
-    # combined file, akin to how it will do it in the host native setup.
-    exit 0
+    cat ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl >> ${COMBINED}
+    ${BROTLI_BIN} -q 5 -o ${OUTPUT} ${COMBINED}
+    rm ${COMBINED}
+
+    # XXX should be safe to remove these at this point
+    rm ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl
 fi
 
-if [ -f "${OBSERVABLES_DIR}/host_info.json" ]; then
-    cat "${OBSERVABLES_DIR}/host_info.json" > "${COMBINED}"
-    echo >> "${COMBINED}"
-fi
 
-cat ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl >> ${COMBINED}
-${BROTLI_BIN} -q 5 -o ${OUTPUT} ${COMBINED}
-rm ${COMBINED}
-# XXX should be safe to remove these at this point
-rm ${OBSERVABLES_DIR}/${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}*.jsonl
